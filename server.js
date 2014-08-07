60b60eb7ca447c87f55fc9da4064107c39c8bd2c
sirjerky <brownad78@gmail.com>
2014-08-07 14:38:07 -0700

    update server.js file

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 var server = http.createServer(app);
+app.set('port', process.env.PORT || 3000);
 
-server.listen(process.env.PORT, function(){
-	console.log('sever has started on port' + process.env.PORT);
+server.listen(app.get('port'), function(){
+	console.log('sever has started on port' + app.get('port'));
 });
